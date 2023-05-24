8b52289057588aeaf024d9e1f600fb53b3909587
the-movie-phimmoi <daiphu094@gmail.com>
2023-05-25 05:14:47 +0700

    cap nhat tt

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import HeroBanner from "@/components/Herobanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
 
-export default function Home() {
+export default function Home({products}) {
   return <main><HeroBanner />
     <Wrapper>
       <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
@@ -16,8 +17,19 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-        <ProductCard/>
+        {
+          products?.data?.map((product)=>(
+            <ProductCard key={product?.id} data={product}/>
+          ))
+        }
       </div>
     </Wrapper>
   </main>
+}
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+      props: { products },
+  };
 }
